1a1e93470e110c51bfc3386b69b714701a7534bb
Anthony Rosas <69225537+AnthonyGit1@users.noreply.github.com>
2024-11-15 06:55:25 -0500

    Configurando el servidor para que escuche desde cualquier lado

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use('/api/match', matchRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
